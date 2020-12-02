43199717c1f31e476fe88c0d7a775416d2c594a0
Tobias Hornberger <saibot1207@googlemail.com>
2020-12-02 10:45:02 +0100

    day02, part2

--- day02/day02.go
+++ day02/day02.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func validpw(inpt string) bool {
+func validpwPart1(inpt string) bool {
 	parsed := strings.Fields(inpt)
 	rangeval := strings.Split(parsed[0], "-")
 	lowVal, _ := strconv.Atoi(rangeval[0])
@@ -17,11 +17,29 @@ func validpw(inpt string) bool {
 	return returnval
 }
 
+func validpwPart2(inpt string) bool {
+	parsed := strings.Fields(inpt)
+	rangeval := strings.Split(parsed[0], "-")
+	lowVal, _ := strconv.Atoi(rangeval[0])
+	highVal, _ := strconv.Atoi(rangeval[1])
+	// if lowVal+1 >= len(parsed[2]) {
+	// 	return false
+	// }
+	// if highVal+1 >= len(parsed[2]) {
+	// 	fmt.Println("debug!")
+	// 	return parsed[2][lowVal+1] == parsed[1][0]
+	// }
+	lowValLow := parsed[2][lowVal-1] == parsed[1][0]
+	highValLow := parsed[2][highVal-1] == parsed[1][0]
+	returnval := (lowValLow || highValLow) && !(lowValLow && highValLow)
+	return returnval
+}
+
 func main() {
 	inputlines := inputloader.ReadInput("../data/input02.txt")
 	sum := 0
 	for _, s := range inputlines {
-		if validpw(s) {
+		if validpwPart2(s) {
 			sum++
 		}
 	}
