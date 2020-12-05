3b1569e54e22e93fa8c83bbdfde4da16d5ab6438
Tobias Hornberger <saibot1207@googlemail.com>
2020-12-05 09:57:49 +0100

    finish day05

--- day05/day05.go
+++ day05/day05.go
@@ -5,6 +5,7 @@ import (
     "strings"
     "fmt"
     "inputloader"
+    "sort"
 )
 
 var replacer = strings.NewReplacer("F", "0", "B", "1", "L", "0", "R", "1")
@@ -17,14 +18,18 @@ func calculateBin(inpt string) int {
 func main() {
     inputlines := inputloader.ReadInput("../data/input05.txt")
     // inputlines := []string{"FBFBBFFRLR", "BFFFBBFRRR", "FFFBBBFRRR", "BBFFBBFRLL"}
-    max := 0
+    var seats []int
     for _, s := range inputlines {
         row := calculateBin(s[:7])
         column := calculateBin(s[7:])
         id := row * 8 + column
-        if id > max {
-            max = id
-        }
+        seats = append(seats, id)
     }
-    fmt.Println(max)
+    sort.Ints(seats)
+    for i := range seats {
+			if seats[i] + 1 != seats[i+1] {
+                fmt.Println(seats[i] + 1)
+                return
+            }
+    }
 }
