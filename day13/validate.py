222ad99b8189f31a2c657955c7d74ec72d4c4585
Tobias Hornberger <saibot1207@googlemail.com>
2020-12-14 13:05:22 +0100

    day 13 python alternative

--- day13/validate.py
+++ day13/validate.py
@@ -1,21 +1,26 @@
 import numpy as np
 
-def gcdExtended(a, b):
-    # Base Case
-    if a == 0 :
-        return b,0,1
+def chinese_remainder(n, a):
+    sum = 0
+    prod = np.prod(n)
+    for i in range(len(n)):
+        p = prod // n[i]
+        sum += a[i] * mul_inv(p, n[i]) * p
+    return sum % prod
 
-    gcd,x1,y1 = gcdExtended(b%a, a)
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1: return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a%b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0: x1 += b0
+    return x1
 
-    # Update x and y using results of recursive
-    # call
-    x = y1 - (b//a) * x1
-    y = x1
-
-    return gcd,x,y
-
-# input = "13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23".split(',')
-input = "1789,37,47,1889".split(',')
+input = "13,x,x,41,x,x,x,x,x,x,x,x,x,467,x,x,x,x,x,x,x,x,x,x,x,19,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,353,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23".split(',')
+# input = "1789,37,47,1889".split(',')
 inputparsed = [(input[i], i) for i in range(len(input))]
 inputparsed2 = list(filter(lambda x: x[0] != 'x', inputparsed))
 
@@ -25,17 +30,13 @@ offsets = [-1 * i[1] for i in inputparsed2]
 print(mods)
 print(offsets)
 
-# mods = [int(x) for x in "13 41 467 19 17 29 353 37 23".split()]
-# offsets = [-1 * int(x) for x in "0 3 13 25 30 42 44 50 67".split()]
 
-m = np.prod(mods)
+res = chinese_remainder(mods, offsets)
+print(res)
+# m = np.prod(mods)
+# z = [m/x for x in mods]
+# y = [gcdExtended(z[i], mods[i])[1]  for i in range(len(mods))]
+# w = [(y[i] * z[i]) % m for i in range(len(mods))]
+# x = [offsets[i] * w[i] for i in range(len(offsets))]
 
-z = [m/x for x in mods]
-y = [gcdExtended(z[i], mods[i])[1]  for i in range(len(mods))]
-
-w = [y[i] * z[i] % m for i in range(len(mods))]
-
-x = [offsets[i] * w[i] for i in range(len(offsets))]
-
-print(sum(x) % m)
 
